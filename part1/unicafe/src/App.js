4897acffeae368233af82ea75540e060bebeab07
Daniel Ivanov Dechev <datas2k@gmail.com>
2022-08-18 19:01:04 +0200

    unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,10 +6,14 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   
+  const total = good + neutral + bad;
+  const average = total ? ((good * 1 + bad * -1) / total) : 0;
+  const positive = total ? `${ ( (good / total) * 100) }` : '0';
+  
   const feedBack = {
     'good': ()=> setGood(good+1),
     'neutral': ()=> setNeutral(neutral+1),
-    'bad': ()=> setBad(bad+1)
+    'bad': ()=> setBad(bad+1),
   }
 
   const Button = ({ feed }) => {
@@ -17,7 +21,7 @@ const App = () => {
       <button className='App-button' onClick={()=> feedBack[`${feed}`]()}>{feed}</button>
     )
   }
- 
+  
   return (
     <div className='App'>
       <h1 className='App-header'>Give Feedback</h1>
@@ -28,9 +32,13 @@ const App = () => {
       </ul>
       <h1 className='App-header'>Statistics</h1>
       <ul className='App-statistics-list'>
-        <li><p>good: {good}</p></li>
-        <li><p>neutral: {neutral}</p></li>
-        <li><p>bad: {bad}</p></li>
+        <li><p>Good: {good}</p></li>
+        <li><p>Neutral: {neutral}</p></li>
+        <li><p>Bad: {bad}</p></li>
+        <li><p>All: {total}</p></li>
+        <li><p>Avarage: {average}</p></li>
+        <li><p>Positive: {positive} %</p></li>
+        
       </ul>
     </div>
   );
