848b09da66625c666407977131bbdb8187e928b0
Daniel Ivanov Dechev <datas2k@gmail.com>
2022-08-18 16:22:24 +0200

    unicafe step1.1

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,20 +1,50 @@
-import { useState } from 'react'
+import './App.css';
+import { useState } from 'react';
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  
+  const handleClick = (data) => {
+    switch (data) {
+      case 'good':
+        setGood(good+1)
+        break;
+      case 'neutral':
+        setNeutral(neutral+1)
+        break;
+      case 'bad':
+        setBad(bad+1)
+        break;
+      default:
+        break;
+    }
+  };
+
+  const Button = ({ feed }) => {
+    return (
+      <button className='App-button' onClick={()=> handleClick(feed)}>{feed}</button>
+    )
+  }
+ 
   return (
-    <div>
-      <h1>Give Feedback</h1>
-      <button>good</button>&nbsp;<button>neutral</button>&nbsp;<button>bad</button>
-      <h1>Stattistics</h1>
-      <p>good</p>
-      <p>neutral</p>
-      <p>bad</p>
+    <div className='App'>
+      <h1 className='App-header'>Give Feedback</h1>
+      <ul className='App-button-list'>
+        <li><Button feed='good'/></li>
+        <li><Button feed='neutral'/></li>
+        <li><Button feed='bad'/></li>
+      </ul>
+      <h1 className='App-header'>Statistics</h1>
+      <ul className='App-statistics-list'>
+        <li><p>good: {good}</p></li>
+        <li><p>neutral: {neutral}</p></li>
+        <li><p>bad: {bad}</p></li>
+      </ul>
     </div>
-  )
+  );
 }
 
 export default App;
