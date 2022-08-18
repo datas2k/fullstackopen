e72cd1abc8a39e5776b0d5bef2d557b41e9fc16b
Daniel Ivanov Dechev <datas2k@gmail.com>
2022-08-18 12:15:24 +0200

    unicafe step1

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,7 +1,20 @@
-function App() {
+import { useState } from 'react'
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
   return (
-    <div className="App"></div>
-  );
+    <div>
+      <h1>Give Feedback</h1>
+      <button>good</button>&nbsp;<button>neutral</button>&nbsp;<button>bad</button>
+      <h1>Stattistics</h1>
+      <p>good</p>
+      <p>neutral</p>
+      <p>bad</p>
+    </div>
+  )
 }
 
 export default App;
